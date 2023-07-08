aa2d5058882b5c6288940c003a982a88bf8eb2e5
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2023-07-08 15:26:15 +0200

    cleaning the code adding Fahrenheit and Celsius  converters

--- file.js
+++ file.js
@@ -37,37 +37,11 @@ function date(dates) {
 let p = document.querySelector("p");
 p.innerHTML = date(now);
 
-function temp(search) {
-  console.log(search.data);
-  let temperate = Math.round(search.data.main.temp);
-  let id = document.getElementById("temperat");
-  id.innerHTML = temperate;
-  let name = search.data.name;
-  let h1 = document.querySelector("#name");
-  h1.innerHTML = name;
-  let hum = search.data.main.humidity;
-  let humin = document.getElementById("humin");
-  humin.innerHTML = `Humidity:${hum}%`;
-  let windy = Math.round(search.data.wind.speed);
-  let speed = document.getElementById("Wind");
-  speed.innerHTML = `Windspeed:${windy}km/h`;
-  let descrip = search.data.weather[0].description;
-  let description = document.getElementById("description");
-  description.innerHTML = `Description:${descrip}`;
-  let icon = search.data.weather[0].icon;
-  let iconn = document.querySelector("#icon");
-  iconn.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
-}
-
-let apiKey = "96ad27349a64ea1dcdfbe6f4d458c085";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Durban&&units=metric`;
-axios.get(`${apiUrl}&appid=${apiKey}`).then(temp);
-
 function temperature(response) {
   console.log(response.data);
-  let temperate = Math.round(response.data.main.temp);
+  celsiusTemp = response.data.main.temp;
   let id = document.getElementById("temperat");
-  id.innerHTML = temperate;
+  id.innerHTML = Math.round(celsiusTemp);
   let name = response.data.name;
   let h1 = document.querySelector("#name");
   h1.innerHTML = name;
@@ -95,8 +69,6 @@ function handleSubmit(event) {
   let city = document.querySelector("#city").value;
   searchCity(city);
 }
-let searchForm = document.querySelector("#search");
-searchForm.addEventListener("submit", handleSubmit);
 
 function search(position) {
   let longitude = position.coords.longitude;
@@ -111,5 +83,31 @@ function getCurrentPosition() {
   navigator.geolocation.getCurrentPosition(search);
 }
 
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  let fahrenheit = document.querySelector("#temperat");
+  fahrenheit.innerHTML = Math.round(fahrenheitTemp);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let fahrenheit = document.querySelector("#temperat");
+  fahrenheit.innerHTML = Math.round(celsiusTemp);
+}
+
+let celsiusTemp = null;
+
+let searchForm = document.querySelector("#search");
+searchForm.addEventListener("submit", handleSubmit);
+
 let button = document.querySelector("button");
 button.addEventListener("click", getCurrentPosition);
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", displayFahrenheit);
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", displayCelsius);
+
+searchCity("Durban");
